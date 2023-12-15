804bbea281233d062ffe40f347c57bbc28e4fe2d
BT_1N3_26 <sample@bths.edu>
2023-12-15 10:44:17 -0500

    third print statement and new class

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!!");
         System.out.println("wish it was a saturday :((");
+        System.out.println("But saturdays are closer to mondays!");
     }
 }
