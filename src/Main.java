472a70e18af985c5e452020bd20d2e283f5d56b5
FNashaat <farheenn@nycstudents.net>
2023-12-15 10:50:57 -0500

    changed ur line hehe

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!!");
+        System.out.println("Hello, my name is Farhana!!");
         System.out.println("wish it was a saturday :((");
         System.out.println("But saturdays are closer to mondays!");
         System.out.println("Ugh ur so right...");
