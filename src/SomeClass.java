6f6aa815e43492f66913bf57a71ecd971cc657b4
BT_1N3_26 <sample@bths.edu>
2023-12-15 10:51:17 -0500

    some classs multiply by 4 method

--- src/SomeClass.java
+++ src/SomeClass.java
@@ -2,4 +2,8 @@ public class SomeClass {
     public static int multiplyBy3(int num) {
         return num * 3;
     }
+
+    public static int multiplyBy4(int num) {
+        return num * 4;
+    }
 }
